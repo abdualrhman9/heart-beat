e35a8e0f6ca40b37e537a0d6817f95f26a3a8c56
abdualrhman9 <abdoalrhman9@hotmail.com>
2021-03-03 11:34:44 -0800

    offline bootstrap files

--- stuff/views/header.php
+++ stuff/views/header.php
@@ -4,7 +4,7 @@
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="dashboard.php">Navbar</a>
+  <a class="navbar-brand" href="dashboard.php">CIS 4 CCU</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
